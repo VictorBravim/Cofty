415a4fda37b9a797a3806f1150d4e19c09352b74
Victor Bravim <victorrafaelbravim@gmail.com>
2024-05-15 11:51:32 -0300

    Mudança

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,13 +4,12 @@ import React from 'react';
 
 export default function Hero() {
     return (
-        <div className="relative h-screen bg-cover bg-center" style={{ backgroundImage: "url('/path-to-your-image.jpg')" }}>
-            <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="relative h-screen bg-cover bg-center mx-12" style={{ backgroundImage: "url('/Banner.png')" }}>
             <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
-                <h1 className="text-5xl font-bold">Seu Texto Aqui</h1>
-                <p className="mt-4 text-lg">Texto descritivo adicional pode ser colocado aqui.</p>
-                <button className="mt-8 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-md">
-                    Clique Aqui
+                <h1 className="text-5xl font-bold">Coffee House</h1>
+                <p className="mt-4 text-2xl">FEEL THE AROMA</p>
+                <button className="mt-8 px-6 py-3 bg-white hover:bg-blue-700 text-black font-semibold">
+                    DISCOVER MORE
                 </button>
             </div>
         </div>
