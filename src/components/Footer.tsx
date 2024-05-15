4d08c52b7465d284e3ddecba6f83d29f31f95b16
Victor Bravim <victorrafaelbravim@gmail.com>
2024-05-14 22:09:26 -0300

    Mudança

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,69 @@
 // Footer.tsx
+'use client'
+import { useState, useEffect } from 'react';
+import Image from "next/image";
+import logo from '@/assets/logo.png'
 
 export default function Footer() {
-    return (
+    const [activeSection, setActiveSection] = useState('home');
 
+    const handleScroll = () => {
+        const scrollPosition = window.scrollY;
+
+        if (scrollPosition < window.innerHeight - 100) {
+            setActiveSection('home');
+        } else if (scrollPosition < 2 * window.innerHeight - 100) {
+            setActiveSection('produtos');
+        } else if (scrollPosition < 3 * window.innerHeight - 100) {
+            setActiveSection('sobre');
+        } else {
+            setActiveSection('contato');
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const scrollToSection = (id: string) => {
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
+    return (
+        <footer className="bg-transparent text-white p-8">
+            <div className="mx-5 flex flex-col lg:flex-row items-center justify-center lg:justify-between">
+                <div className="flex items-center mb-2 lg:mb-0">
+                    <Image src={logo} alt="Texto" width={150} height={32} />
+                </div>
+                <div className="md:flex space-x-2 lg:space-x-12 mt-4 md:mt-0">
+                    <button
+                        className={`text-base md:text-lg ${activeSection === 'home' ? 'bg-transparent text-white py-2 border-b-2 border-white' : ''}`}
+                        onClick={() => scrollToSection('home')}>
+                        HOME
+                    </button>
+                    <button
+                        className={`text-base md:text-lg ${activeSection === 'produtos' ? 'bg-transparent text-white py-2 border-b-2 border-white' : ''}`}
+                        onClick={() => scrollToSection('produtos')}>
+                        PRODUTOS
+                    </button>
+                    <button
+                        className={`text-base md:text-lg ${activeSection === 'sobre' ? 'bg-transparent text-white py-2 border-b-2 border-white' : ''}`}
+                        onClick={() => scrollToSection('sobre')}>
+                        SOBRE
+                    </button>
+                    <button
+                        className={`text-base md:text-lg ${activeSection === 'contato' ? 'bg-transparent text-white py-2 border-b-2 border-white' : ''}`}
+                        onClick={() => scrollToSection('contato')}>
+                        CONTATO
+                    </button>
+                </div>
+            </div>
+        </footer>
     );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,48 @@
 // Hero.tsx
+'use client'
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Image from "next/image";
+import banner1 from '@/assets/banner1.png'
+import banner2 from '@/assets/banner2.png'
+import banner3 from '@/assets/banner3.png'
 
 export default function Hero() {
-    return (
+    const sliderSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        adaptiveHeight: false,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                },
+            },
+        ],
+    };
 
+    return (
+        <div id='home' className="w-full px-8 lg:px-12">
+            <Slider {...sliderSettings}>
+                <div className='shadow-lg'>
+                    <Image src={banner1} className="bg-white w-full h-1/2 object-cover pointer-events-none" alt="Slide 1" />
+                </div>
+                <div className='shadow-lg'>
+                    <Image src={banner2} className="bg-white w-full h-1/2 object-cover pointer-events-none" alt="Slide 2" />
+                </div>
+                <div className='shadow-lg'>
+                    <Image src={banner3} className="bg-white w-full h-1/2 object-cover pointer-events-none" alt="Slide 3" />
+                </div>
+            </Slider>
+        </div>
     );
 }
